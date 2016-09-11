5fac3192587d361c3f0213867b22a815aa5973dd
余洪春 <yuhongchun027@gmail.com>
2016-09-11 14:58:09 +0800

    Update init.pp

--- init.pp
+++ init.pp
@@ -1,4 +1,4 @@
-#此init.pp文件为puppet的apache模块中的文件，以此文件举例说明了git和github的用法
+#此init.pp文件为puppet的apache模块中的文件，以此文件举例说明了github的用法，原先的版本库已改名，由security改名为avaliablity。
 class apache{
         package{"httpd":
         ensure          =>present,
